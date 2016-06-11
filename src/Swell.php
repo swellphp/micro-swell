02bcfbcd146c5c4990c8243eafcd3b8e0907a67f
Keith Mifsud <mifsud.k@gmail.com>
2016-06-11 22:13:51 +0000

    git-2 Just sorting things out

--- src/Swell.php
+++ src/Swell.php
@@ -1,8 +1,26 @@
-<?php
+<?php declare (strict_types = 1);
 
-namespace Swell;
+/**
+ * This file is part of Swell Php Micro-framework (micro-swell)
+ *
+ * @licence Please view the Licence file supplied with this source code.
+ *
+ * @author Keith Mifsud <http://www.keith-mifsud.com>
+ *
+ * @copyright Keith Mifsud 2016
+ *
+ * @version 1.0 First Release
+ */
 
-class Swell 
+namespace Swell\Micro;
+
+
+/**
+ * @ToDo something, anything!!!
+ *
+ * @package Swell\Micro
+ */
+class Swell
 {
 
 }
