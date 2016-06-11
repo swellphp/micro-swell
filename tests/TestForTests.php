80b6cca1032a184329a454ef2cb53ed8d9a56ecc
Keith Mifsud <mifsud.k@gmail.com>
2016-06-11 22:01:48 +0000

    git-2 added a test for tests

---
+++ tests/TestForTests.php
@@ -0,0 +1,30 @@
+<?php declare (strict_types = 1);
+
+/**
+ * This file is part of Swell Php Micro-framework (micro-swell)
+ *
+ * @licence Please view the Licence file supplied with this source code.
+ *
+ * @author Keith Mifsud <http://www.keith-mifsud.com>
+ *
+ * @copyright Keith Mifsud 2016
+ *
+ * @version 1.0 First Release
+ */
+
+namespace Swell\Micro\Tests;
+
+
+/**
+ * Tests that the test suite works
+ *
+ * @package Swell\Micro\Tests
+ */
+class TestForTests extends \PHPUnit_Framework_TestCase
+{
+    public function testTestingSuite()
+    {
+        $this->assertTrue(true);
+    }
+
+}
